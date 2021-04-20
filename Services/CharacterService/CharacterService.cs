e67871623002fdfb58ca2e234c652f483ff790ca
Basilis Argyroglou <b.argyroglou@metronsa.eu>
2021-04-20 12:43:13 +0300

    ServiceResponse Implemetation

--- Services/CharacterService/CharacterService.cs
+++ Services/CharacterService/CharacterService.cs
@@ -12,20 +12,26 @@ namespace dotnet_rpg_updated.Services.CharacterService
             new Character(),
             new Character { Id = 1, Name = "Sam"}
         };
-        public async Task<List<Character>> AddCharacter(Character newCharacter)
+        public async Task<ServiceResponse<List<Character>>> AddCharacter(Character newCharacter)
         {
+            var serviceRespone = new ServiceResponse<List<Character>>();
             characters.Add(newCharacter);
-            return characters;
+            serviceRespone.Data = characters;
+            return serviceRespone;
         }
 
-        public async Task<List<Character>> GetAllCharacters()
+        public async Task<ServiceResponse<List<Character>>> GetAllCharacters()
         {
-            return characters;
+            var serviceRespone = new ServiceResponse<List<Character>>();
+            serviceRespone.Data = characters;
+            return serviceRespone;
         }
 
-        public async Task<Character> GetCharacterById(int id)
+        public async Task<ServiceResponse<Character>> GetCharacterById(int id)
         {
-            return characters.FirstOrDefault(c => c.Id == id);
+            var serviceRespone = new ServiceResponse<Character>();
+            serviceRespone.Data = characters.FirstOrDefault(c => c.Id == id);
+            return serviceRespone;
         }
     }
 }
