866b403df515230cc8182bdb8f1dfc2e7d5e6588
Basilis Argyroglou <b.argyroglou@metronsa.eu>
2021-04-21 10:09:41 +0300

    Implemet Authentication

---
+++ Controllers/AuthController.cs
@@ -0,0 +1,33 @@
+using System.Threading.Tasks;
+using dotnet_rpg_updated.Data;
+using dotnet_rpg_updated.Dtos.User;
+using dotnet_rpg_updated.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace dotnet_rpg_updated.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class AuthController : ControllerBase
+    {
+        private readonly IAuthRepository _authRepo;
+        public AuthController(IAuthRepository authRepo)
+        {
+            _authRepo = authRepo;
+        }
+
+        [HttpPost("Register")]
+        public async Task<ActionResult<ServiceResponse<int>>> Register(UserRegisterDto request)
+        {
+            var response = await _authRepo.Register(
+                new User { Username = request.Username }, request.Password
+            );
+            if (!response.Success)
+            {
+                return BadRequest(response);
+            }
+
+            return Ok(response);
+        }
+    }
+}
