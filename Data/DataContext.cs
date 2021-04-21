ec20d501b44bdf6aef213397d0f4954e706572da
Basilis Argyroglou <b.argyroglou@metronsa.eu>
2021-04-21 08:55:39 +0300

    Add ConectionString with Db and configure it in start up

---
+++ Data/DataContext.cs
@@ -0,0 +1,15 @@
+using dotnet_rpg_updated.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace dotnet_rpg_updated.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Character> Characters { get; set; }
+    }
+}
