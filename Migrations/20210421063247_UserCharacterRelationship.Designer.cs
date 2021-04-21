fea42630c414d3f2704ea18e7a2a83f801c1dabb
Basilis Argyroglou <b.argyroglou@metronsa.eu>
2021-04-21 09:34:51 +0300

    First RelationShip implementation Run Migrations and upadte db

--- Migrations/20210421063247_UserCharacterRelationship.Designer.cs
+++ Migrations/20210421063247_UserCharacterRelationship.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using dotnet_rpg_updated.Data;
 
 namespace dotnet_rpg_updated.Migrations
 {
     [DbContext(typeof(DataContext))]
-    partial class DataContextModelSnapshot : ModelSnapshot
+    [Migration("20210421063247_UserCharacterRelationship")]
+    partial class UserCharacterRelationship
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
