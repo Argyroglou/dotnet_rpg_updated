28875da84b8c5fdcb52697f9305f6a7cb30db0cd
Basilis Argyroglou <b.argyroglou@metronsa.eu>
2021-04-22 10:11:15 +0300

    Fight implementation

---
+++ Dtos/Fight/FightRequestDto.cs
@@ -0,0 +1,9 @@
+using System.Collections.Generic;
+
+namespace dotnet_rpg_updated.Dtos.Fight
+{
+    public class FightRequestDto
+    {
+        public List<int> CharacterIds { get; set; }
+    }
+}
