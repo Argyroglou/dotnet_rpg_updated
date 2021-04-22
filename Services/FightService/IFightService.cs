b412bbeaea979a32b524301c391d18e2e21f484f
Basilis Argyroglou <b.argyroglou@metronsa.eu>
2021-04-22 09:35:58 +0300

    Attack with skills implementation

--- Services/FightService/IFightService.cs
+++ Services/FightService/IFightService.cs
@@ -7,5 +7,6 @@ namespace dotnet_rpg_updated.Services.FightService
     public interface IFightService
     {
         Task<ServiceResponse<AttackResultDto>> WeaponAttack(WeaponAttackDto request);
+        Task<ServiceResponse<AttackResultDto>> SkillAttack(SkillAttackDto request);
     }
 }
